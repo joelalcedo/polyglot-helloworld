edaf80912a91aace4f677f0c89d663ba8c0e307e
joel <joelalcedo@outlook.com>
2025-12-18 08:11:56 -0500

    Added languages
    Added support for many more languages

---
+++ languages/boomerang/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IMG="hello-boomerang"
+PLATFORM="${POLYGLOT_PLATFORM:-}"
+if [ -n "$PLATFORM" ]; then
+  docker build --platform "$PLATFORM" -t "$IMG" .
+  docker run --rm --platform "$PLATFORM" "$IMG"
+else
+  docker build -t "$IMG" .
+  docker run --rm "$IMG"
+fi
